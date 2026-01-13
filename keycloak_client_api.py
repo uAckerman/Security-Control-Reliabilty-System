72ea5bdba50c1b1918a17be3dc8c2b68ea2d102e
Uzair Khan <never.regretonthechoiceyoumade@gmail.com>
2026-01-13 23:58:53 +0500

    Add files via upload

---
+++ keycloak_client_api.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_ADMIN_URL = "http://localhost:8080/admin/realms/scrs-core"
+
+class KeycloakClient:
+    def __init__(self, token):
+        self.headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+    def get_events(self):
+        url = f"{BASE_ADMIN_URL}/events"
+        r = requests.get(url, headers=self.headers)
+        r.raise_for_status()
+        return r.json()
